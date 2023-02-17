be7d438dd6bacf2e64b9707afea6711f77c30c86
root <Oghenero>
2023-02-17 07:34:00 -0800

    5-printf.c done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/**
+ * main -Entry point
+ * A C program using "printf" to print a string.
+ * return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
