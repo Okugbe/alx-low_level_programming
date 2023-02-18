c78a4165cd52e54559a2a5c1505aa1ce00254a12
root <Oghenero>
2023-02-18 15:17:33 -0800

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - alphabet backwards
+ *
+ * Return Always 0 (Success)
+ */
+int main(void)
+{
+	char back
+		;
+	back = 'z';
+	while (back >= 'a')
+	{
+		putchar(back);
+		back--;
+	}
+	putchar('\n');
+	return (0);
+}
