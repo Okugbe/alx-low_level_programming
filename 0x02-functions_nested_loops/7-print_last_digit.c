f3137d2c149523850c907f66a8a7408c3037dada
root <Oghenero>
2023-02-22 13:58:43 -0800

    7-last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - print the last digit
+ * @n: the number
+ * Return: value of the last digit
+ */
+int print_last_digit(int n)
+{
+int z = n % 10;
+
+if (z < 0)
+{
+	z = z * -1;
+}
+_putchar(z + '0');
+return (0);
+}
