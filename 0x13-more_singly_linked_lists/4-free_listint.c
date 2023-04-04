df4751f6386e75b1889804112245ddaa985e9560
root <Oghenero>
2023-04-04 14:10:27 -0700

    function that frees a listint_t list re-up

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -6,12 +6,12 @@
  */
 void free_listint(listint_t *head)
 {
-    listint_t *temp;
+listint_t *temp;
 
-    for (temp = head; head != NULL; temp = head)
-    {
-        head = head->next;
-        free(temp);
-    }
+for (temp = head; head != NULL; temp = head)
+{
+	head = head->next;
+	free(temp);
+}
 }
 
